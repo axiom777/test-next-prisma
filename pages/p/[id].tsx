023afc4afcc2d5eae4365255fb59480f94cc0058
axiom777 <axiom777@yandex.ru>
2021-07-06 15:34:11 +0300

    Add Delete functionality

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { GetServerSideProps } from 'next';
-import ReactMarkdown from 'react-markdown';
-import Layout from '../../components/Layout';
-import Router from 'next/router';
-import { PostProps } from '../../components/Post';
-import { useSession } from 'next-auth/client';
-import prisma from '../../lib/prisma';
+import React from "react";
+import { GetServerSideProps } from "next";
+import ReactMarkdown from "react-markdown";
+import Layout from "../../components/Layout";
+import Router from "next/router";
+import { PostProps } from "../../components/Post";
+import { useSession } from "next-auth/client";
+import prisma from "../../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
@@ -25,9 +25,16 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 async function publishPost(id: number): Promise<void> {
   await fetch(`http://localhost:4000/api/publish/${id}`, {
-    method: 'PUT',
+    method: "PUT",
   });
-  await Router.push('/');
+  await Router.push("/");
+}
+
+async function deletePost(id: number): Promise<void> {
+  await fetch(`http://localhost:4000/api/post/${id}`, {
+    method: "DELETE",
+  });
+  Router.push("/");
 }
 
 const Post: React.FC<PostProps> = (props) => {
@@ -46,10 +53,13 @@ const Post: React.FC<PostProps> = (props) => {
     <Layout>
       <div>
         <h2>{title}</h2>
-        <p>By {props?.author?.name || 'Unknown author'}</p>
+        <p>By {props?.author?.name || "Unknown author"}</p>
         <ReactMarkdown source={props.content} />
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(props.id)}>Delete</button>
         )}
       </div>
       <style jsx>{`
